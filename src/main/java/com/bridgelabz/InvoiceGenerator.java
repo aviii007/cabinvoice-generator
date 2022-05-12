f7c0a8021cd091b3571e4f660275567088077bbd
divyanaC999 <divyachourasiya999@gmail.com>
2022-05-12 15:54:44 +0530

    Added: the for calculating multiple rides and ride class

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -8,8 +8,16 @@ public class InvoiceGenerator {
 
     public double calculateFare(double distance, int time) {
         double totalFare = (distance * COST_PER_KM) + (time * COST_PER_TIME);
-        if(totalFare < MINIMUM_FARE){
+        if (totalFare < MINIMUM_FARE) {
             return MINIMUM_FARE;
+        }
+        return totalFare;
+    }
+
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
         }
         return totalFare;
     }
