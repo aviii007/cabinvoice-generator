5bfa8d2530214d41a8ae882df31b7ee32de2388a
divyanaC999 <divyachourasiya999@gmail.com>
2022-05-12 17:06:11 +0530

    Added: User id and rides will return invoice summary

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -34,8 +34,21 @@ public class InvoiceServiceTest {
 
         InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
         InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
-        Assert.assertEquals(expectedInvoiceSummary,summary);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
 
+    }
+
+    @Test
+    public void givenUserIdAndRides_ShouldReturnInvoiceSummary() {
+        String user1 = "divya";
+        Ride[] rides1 = {new Ride(2.0, 5), new Ride(0.1, 1)};
+        invoiceService.addRides(user1, rides1);
+        String user2 = "Chourasiya";
+        Ride[] rides2 = {new Ride(3.0, 5), new Ride(0.1, 1)};
+        invoiceService.addRides(user2, rides2);
+        InvoiceSummary summary = invoiceService.getInvoiceSummary(user1);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30);
+        Assert.assertEquals(expectedInvoiceSummary, summary);
     }
 }
 
