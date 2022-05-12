7664fd17e60dc0c13404fe5f629eae4084f9e6da
divyanaC999 <divyachourasiya999@gmail.com>
2022-05-12 13:23:46 +0530

    Added: test case less distance and time returns true

---
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class InvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare);
+
+    }
+
+    @Test
+    public void givenLessDistanceAndTime_ShouldReturnTrue() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(5, fare);
+    }
+}
+
