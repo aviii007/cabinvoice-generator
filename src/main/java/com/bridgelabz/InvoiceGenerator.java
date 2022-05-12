582c64f66339d6805a845adee8ba3aa7a23672b2
divyanaC999 <divyachourasiya999@gmail.com>
2022-05-12 12:54:35 +0530

    ADDED: The cost per KM, cost per time and minimum fare

--- src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -1,4 +1,10 @@
 package com.bridgelabz;
 
 public class InvoiceGenerator {
+    private static final double MINIMUM_COST_PER_KM = 10;
+    private static final int COST_PER_TIME = 1;
+    private static final double MINIMUM_FARE = 5;
+
+
+
 }
