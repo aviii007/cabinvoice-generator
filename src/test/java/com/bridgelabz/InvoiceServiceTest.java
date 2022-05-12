dc5ac0d0ca2fde0dbba8e547ff5062949079fe3a
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-05-12 12:39:08 +0530

    added test case for given distance and time should return the total fare

---
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class InvoiceServiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assertions.assertEquals(25, fare);
+    }
+}
